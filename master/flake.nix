639c2018fe059b1dbc6ec49a28f2f94da3bea23c
hugosenari <hugosenari@gmail.com>
2022-03-27 21:49:47 -0300

    chore: re index fwrite

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-fwrite-master.ref   = "refs/heads/master";
   inputs.src-fwrite-master.owner = "pdrb";
   inputs.src-fwrite-master.repo  = "nim-fwrite";
-  inputs.src-fwrite-master.dir   = "";
   inputs.src-fwrite-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
