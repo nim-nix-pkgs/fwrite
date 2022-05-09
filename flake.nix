4cd3598e1da6663503bb784b5b39a113e41afbb2
hugosenari <hugosenari@gmail.com>
2022-05-09 20:37:31 -0300

    chore: re index fwrite

--- flake.nix
+++ flake.nix
@@ -7,22 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."fwrite-master".dir   = "master";
-  inputs."fwrite-master".owner = "nim-nix-pkgs";
-  inputs."fwrite-master".ref   = "master";
-  inputs."fwrite-master".repo  = "fwrite";
-  inputs."fwrite-master".type  = "github";
-  inputs."fwrite-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."fwrite-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."fwrite-v1_0_0".dir   = "v1_0_0";
-  inputs."fwrite-v1_0_0".owner = "nim-nix-pkgs";
-  inputs."fwrite-v1_0_0".ref   = "master";
-  inputs."fwrite-v1_0_0".repo  = "fwrite";
-  inputs."fwrite-v1_0_0".type  = "github";
-  inputs."fwrite-v1_0_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."fwrite-v1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
